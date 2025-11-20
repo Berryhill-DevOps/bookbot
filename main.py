62b03f805ddfff985291f4ad41f6392d409817f9
KingCobraa-25 <johnberryhill25@gmail.com>
2025-11-20 07:39:49 -0600

    added and modified code to both main.py & stats.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, sort_char_count
 
 def get_book_text(book):
     with open(book, 'r', encoding="utf-8") as f:
@@ -9,7 +9,9 @@ def get_book_text(book):
 def main():
     num_words = get_word_count(get_book_text(book_filepath))
     num_chars = get_char_count(get_book_text(book_filepath))
-    return print(f"Found {num_words} total words & {num_chars}")
+    sorted_chars = sort_char_count(num_chars)
+
+    # add here: the report layout! almost done! no return needed for this function
 
 book_filepath = "books/frankenstein.txt"
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,22 @@ def get_char_count(file_contents):
         else:
             chars[ch] += 1
     return chars
+
+# helper function for sorting
+def sort_on(items):
+    return items["num"]
+
+def sort_char_count(char_dict):
+    char_counts = []
+    for ch in char_dict:
+        new_dict = {}
+        new_dict["char"] = ch # first key:value pair: "char: a" for example
+        
+        value = char_dict[ch]
+        new_dict["num"] = value # second key:value pair: "num: 1000" for example
+        
+        char_counts.append(new_dict)
+    
+    char_counts.sort(reverse=True, key=sort_on)
+
+    return char_counts
