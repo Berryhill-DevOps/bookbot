f3eaa9d02634b3d42f54c450c6f4a4a89a7539e5
KingCobraa-25 <johnberryhill25@gmail.com>
2025-11-20 18:02:45 -0600

    added code to main.py to check for proper commands

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count, get_char_count, sort_char_count
 
 def get_book_text(book):
@@ -7,11 +8,23 @@ def get_book_text(book):
     return file_contents
 
 def main():
+    book_filepath = sys.argv[1]
     num_words = get_word_count(get_book_text(book_filepath))
     num_chars = get_char_count(get_book_text(book_filepath))
     sorted_chars = sort_char_count(num_chars)
 
-    # add here: the report layout! almost done! no return needed for this function
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_chars:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
 
-book_filepath = "books/frankenstein.txt"
+if len(sys.argv) != 2:
+    print(f"Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 main()
